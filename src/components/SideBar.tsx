a449f79245f6deedbfbe3df5ac5e36abd6ba1f7d
Davi Freitas da Silva <davi.freitas@USEALL.CORP>
2021-03-29 15:56:33 -0300

    Components removed from app.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SidebarProps {
+  handleClick: (id: number) => void;
+  genres: GenreResponseProps[];
+  selectedGenreId: number
+}
+
+export function SideBar({ handleClick, genres, selectedGenreId } : SidebarProps ) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClick(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
 }
